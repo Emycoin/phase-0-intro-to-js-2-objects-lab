1249e62c8a80606b6e1928cfdcde031ffb58d711
Emycoin <emytausevich@gmail.com>
2021-06-19 13:17:36 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+  name: "Emy",
+  streetAddress: "64 Stanhope Street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return Object.assign({}, employee, { [key]: value });
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  return Object.assign(employee, { [key]: value });
+}
+function deleteFromEmployeeByKey(employee, key) {
+  //to clone any object
+  // Object.assign({}, theObjectToCopy)
+  const clonedEmployeeObj = Object.assign({}, employee);
+  delete clonedEmployeeObj[key];
+  return clonedEmployeeObj;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
